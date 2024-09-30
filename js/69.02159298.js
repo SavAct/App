f06ddd43b78c2a36a8b679610cb9d520515e2a0c
Kevin Kei Tuncer <kevin.kei.tuncer@rwth-aachen.de>
2024-09-30 19:48:52 +0900

    Fix going back in tabs

--- js/69.02159298.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis["webpackChunksavactapp"]=globalThis["webpackChunksavactapp"]||[]).push([[69],{48069:(a,e,p)=>{p.r(e),p.d(e,{default:()=>u});var t=p(61758);function n(a,e,p,n,s,c){return(0,t.uX)(),(0,t.CE)("div")}const s=(0,t.pM)({name:"page-empty"});var c=p(12807);const r=(0,c.A)(s,[["render",n]]),u=r}}]);
